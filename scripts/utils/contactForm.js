3fbfe40f4a106735751364a2333380c4d747481b
[PovotSerge] <popovapt@mail.ru>
2021-11-20 18:59:46 +0100

    Update files

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,18 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "block";
+
+    const containeur = document.querySelector('.containeur');
+
+    containeur.style.display = "none";
+
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+
+    const containeur = document.querySelector('.containeur');
+
+    containeur.style.display = "block";
 }
